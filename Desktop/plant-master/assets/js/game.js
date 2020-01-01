3013cd418586c410ba9cf3f80bf20660249486b1
于昕卓 <xinzhuoyu@us-macbook-puro.home>
2020-01-01 20:08:42 +1100

    first commit

--- Desktop/plant-master/assets/js/game.js
+++ Desktop/plant-master/assets/js/game.js
@@ -1,8 +1,7 @@
-
 var userPlant = {  
     userID: 3,
     plantID: 1,
-    growth: 3,
+    growth: 20,
     health: 1,
     waterLevel: 2,
     nutritionLevel: 1,
@@ -10,19 +9,22 @@ var userPlant = {
     daysToGrow: 47,
     dailyWatering: 3,
     weeklyNutrition: 5,
-    lastWatered: Date('26-08-2019'),
+    lastWatered: Date('26-08-2019')
 };
 
+
 function load() {
-    // to be changed with database call
-    // userPlant = <?php echo userPlant?>
+//     to be changed with database call
+//     userPlant = <?php echo userPlant?>
+    
     userPlant.growth = userPlant.growth + userPlant.daysToGrow/(60 * 24);
     console.log(userPlant.growth);
 
     // choose picture resource according to growth and plant id
     var stage = Math.floor((userPlant.growth / 4) + 1);
-    document.getElementById("plant").src = `assets/img/plants/${userPlant.plantID}-${stage}.png`;
-
+    document.getElementById("plant").src = `assets/img/plants/${userPlant.plantName}/${stage}.png`;
+console.log(`assets/img/plants/${userPlant.plantName}/${stage}.png`);
+    
     // display current health
     console.log("Health: " + userPlant.health);
 
@@ -36,12 +38,37 @@ function load() {
 }
 
 function waterPlant() {
-    userPlant.waterLevel = userPlant.waterLevel == userPlant.dailyWatering ? userPlant.waterLevel : userPlant.waterLevel + 1;
-    userPlant.lastWatered = Date();
-
-    // trigger animation
-    // post request to server to save waterLevel   
+    //verify the date 
+    
+    //change 3 picture
+    //change water level and save to database 
 }
 
-function applyFertiliser() {
+
+window.onload=function(){
+    document.getElementById("pot_btn").addEventListener("click",function(){
+            if(level>1){level++;} 
+            if (level>3){level=3;}
+        },false);
+    
+    document.getElementById("fertilizer_btn").addEventListener("click",function(){
+            if(level>1){level++;} 
+            if (level>4){level=4;}
+        },false);
 }
+
+var level = 2;
+function changeGlass() {
+    var glassObj = document.getElementById("glass")
+    glassObj.src = "assets/img/glasses/" + level + ".png";
+}
+
+
+var fert = 2;
+function changeFertiliser() {
+    var glassObj = document.getElementById("plant")
+    glassObj.src = "assets/img/plants/chive/" + fert + ".png";
+}
+window.onload=function(){
+        
+    }
